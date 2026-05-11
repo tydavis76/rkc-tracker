a47b317604ec6f2460af07444c6e87b7f91b009c
Ty Davis <tydavis76@gmail.com>
2026-05-10 20:02:06 -0500

    Isolate rkc-tracker at root and remove kb-fitness

--- kb-fitness/src/hooks/useSettings.ts
+++
@@ -1,7 +0,0 @@
-import { useLiveQuery } from 'dexie-react-hooks'
-import { db } from '../db/db'
-import type { SettingsRecord } from '../db/types'
-
-export function useSettings(): SettingsRecord | undefined {
-  return useLiveQuery(() => db.settings.get(1))
-}
--- kb-fitness/src/utils/programUtils.ts
+++
@@ -1,2 +0,0 @@
-/** Intensity labels cycling pattern for weeks within a phase. */
-export const PHASE_INTENSITIES = ['100%', '105%', '110%', 'deload']
